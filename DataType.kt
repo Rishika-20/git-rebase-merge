6b28eb84676baf5e8ad90912cdfce1017ababec7
Rishika0101 <rishika@example.com>
2023-05-10 12:43:40 +0530

    First commit datatype

---
+++ DataType.kt
@@ -0,0 +1,18 @@
+package basics
+    fun main() {
+
+        // Immutable
+
+        val message = " Rishika"
+        print(message)
+        // hence it is showing error coz, val is immutable once defined cann't be changed.
+      //  message = "Riya"
+
+        // Mutable
+        var message1 = "  Hello Kotlin"
+        println(message1)
+
+        message1 = "have fun"
+        println(message1)
+
+    }
